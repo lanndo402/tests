ad7fdd24e824c528732e504a9b518631cae86922
lanndo402 <lanndo402@gmail.com>
2026-09-17 00:25:40 +0700

    update ex2

--- lesson-03/ex2.js
+++ lesson-03/ex2.js
@@ -1,7 +1,12 @@
 const chieuCao = 180;
 if (100 < chieuCao && chieuCao < 200) {
-    console.log("Cân nặng tối đa: "   + (chieuCao - 100) + "kg");
-    console.log("Cân nặng lý tưởng: " +((chieuCao -100)*9/10) + "kg");
-    console.log("Cân nặng tối thiểu: " +((chieuCao -100)*8/10)+ "kg");
+    
+    const soLe = chieuCao - 100;
+    const canNangToiDa = soLe;
+    const canNangLyTuong = soLe * 9 / 10;
+    const canNangToiThieu = soLe * 8 / 10;
 
+    console.log(("Cân nặng tối đa: "   + canNangToiDa + "kg"),
+    ("Cân nặng lý tưởng: " + canNangLyTuong+ "kg"),
+    ("Cân nặng tối thiểu: " + canNangToiThieu+ "kg"));
 }
